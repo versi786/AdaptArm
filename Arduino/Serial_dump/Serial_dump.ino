b861810db6b1b897a5bdec669e3b580d0ad993d2
Aasif Versi <versia@seas.upenn.edu>
2015-06-20 15:08:25 -0400

    tested new code with LED indication light

--- Arduino/Serial_dump/Serial_dump.ino
+++ Arduino/Serial_dump/Serial_dump.ino
@@ -1,4 +1,4 @@
-#define data_length 1000 //2 seconds
+#define data_length 500 
 
 //Serial Read Variables
 char inData[20]; // Allocate some space for the string
@@ -8,7 +8,7 @@ byte index = 0; // Index into array; where to store the character
 int data[data_length];
 int emg_index = 0;
 int emg_pin = A0;
-
+int LED_pin = 13;
 
 
 //Timing Variables
@@ -18,6 +18,8 @@ void setup(){
  pinMode(emg_pin, INPUT);
  Serial.begin(9600);
  Serial.println("start");
+ digitalWrite(LED_pin, LOW);
+ 
 }
 
 
@@ -37,6 +39,7 @@ String readSerial(){
 }
 
 void dump(){
+  digitalWrite(LED_pin, HIGH);
    for(int i = 0; i < data_length; i++){
       Serial.println(data[i], DEC); // check
    } 
@@ -57,14 +60,15 @@ boolean delay_without_delaying(unsigned long &since, unsigned long time) {
 
 void loop()
 {
-  Serial.print("Hi");
-//  if(emg_index != data_length){
-//    if(delay_without_delaying(emg_time,2)){
-//       //data[emg_index] = analogRead(emg_pin);
-//       emg_index = (emg_index + 1);
-//    }
-//  }else{
-//   emg_index = 0;
-//   dump(); 
-//  }
+  Serial.print("Hi\r\n");
+  digitalWrite(LED_pin, LOW);
+  if(emg_index != data_length){
+    if(delay_without_delaying(emg_time,2)){
+       //data[emg_index] = analogRead(emg_pin);
+       emg_index = (emg_index + 1);
+    }
+  }else{
+   emg_index = 0;
+   dump(); 
+  }
 }
